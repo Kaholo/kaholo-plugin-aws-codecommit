efefd2539e43de32fda0864ee0add8c878d6606d
maciek <42122011+xkcm@users.noreply.github.com>
2022-04-13 19:30:46 +0200

    Add linting (#5)
    * Add linter, fix linting errors
    
    * Update config.json

--- autocomplete.js
+++ autocomplete.js
@@ -1,71 +1,83 @@
 const parsers = require("./parsers");
 
-const CodeCommitService = require('./aws.codecommit.service');
+const CodeCommitService = require("./aws.codecommit.service");
+
 const MAX_RESULTS = 10;
 const MISSING_OR_INCORRECT_CREDENTIALS_ERROR = "Missing or incorrect credentials - please select valid access and secret keys first";
 
 // auto complete helper methods
 
-function mapAutoParams(autoParams){
+function mapAutoParams(autoParams) {
   const params = {};
-  autoParams.forEach(param => {
+  autoParams.forEach((param) => {
     params[param.name] = parsers.autocomplete(param.value);
   });
   return params;
 }
 
-/***
+/** *
  * @returns {[{id, value}]} filtered result items
- ***/
- function handleResult(result, query, keyField, valField){
-  if (!result || result.length === 0) return [];
-  const items = result.map(item =>
-    getAutoResult(keyField ? item[keyField] : item, keyField ? item[valField] : item));
+ ** */
+function handleResult(result, query, keyField, valField) {
+  if (!result || result.length === 0) {
+    return [];
+  }
+  const items = result.map(
+    (item) => getAutoResult(keyField ? item[keyField] : item, keyField ? item[valField] : item),
+  );
   return filterItems(items, query);
 }
 
-/***
+/** *
  * @returns {{id, value}} formatted autocomplete item
- ***/
+ ** */
 function getAutoResult(id, value) {
   return {
     id: id || value,
-    value: value || id
+    value: value || id,
   };
 }
 
-function filterItems(items, query){
-  if (query){
-    const qWords = query.split(/[. ]/g).map(word => word.toLowerCase()); // split by '.' or ' ' and make lower case
-    items = items.filter(item => qWords.every(word => item.value.toLowerCase().includes(word)));
-    items = items.sort((word1, word2) => word1.value.toLowerCase().indexOf(qWords[0]) - word2.value.toLowerCase().indexOf(qWords[0]));
+function filterItems(items, query) {
+  let filteredItems = items;
+  if (query) {
+    const qWords = query.split(/[. ]/g).map((word) => word.toLowerCase()); // split by '.' or ' ' and make lower case
+    filteredItems = filteredItems.filter((item) => (
+      qWords.every((word) => item.value.toLowerCase().includes(word))
+    ));
+    filteredItems = filteredItems.sort((word1, word2) => (
+      word1.value.toLowerCase().indexOf(qWords[0]) - word2.value.toLowerCase().indexOf(qWords[0])
+    ));
   }
-  return items.splice(0, MAX_RESULTS);
+  return filteredItems.splice(0, MAX_RESULTS);
 }
 
 function listAuto(listFuncName, outputName, fields = []) {
   return async (query, pluginSettings, triggerParameters) => {
-    const settings = mapAutoParams(pluginSettings), params = mapAutoParams(triggerParameters);
+    const settings = mapAutoParams(pluginSettings); const
+      params = mapAutoParams(triggerParameters);
     const client = CodeCommitService.from(params, settings);
-    var nextToken, items = [];
+    let nextToken; const
+      items = [];
     do {
-      var result = await client[listFuncName]({
+      // eslint-disable-next-line no-await-in-loop
+      const result = await client[listFuncName]({
         ...params,
-        nextToken
+        nextToken,
       });
       items.push(...handleResult(result[outputName], query, ...fields));
       nextToken = result.nextToken || result.NextToken || result.Marker;
     } while (nextToken && items.length < MAX_RESULTS);
     return items;
-  }
+  };
 }
 
 async function listRegions(query, pluginSettings, actionParams) {
-  let [  settings, params  ] = [ mapAutoParams(pluginSettings), mapAutoParams(actionParams) ];
-  params = { ...params, region: params.region || "eu-west-2" };
+  const [settings, params] = [mapAutoParams(pluginSettings), mapAutoParams(actionParams)];
+  const resolvedParams = { ...params, region: params.region || "eu-west-2" };
   let client;
   try {
-    client = CodeCommitService.from(params, settings);
+    client = CodeCommitService.from(resolvedParams, settings);
   } catch (e) {
     throw MISSING_OR_INCORRECT_CREDENTIALS_ERROR;
   }
@@ -74,18 +86,20 @@ async function listRegions(query, pluginSettings, actionParams) {
   const lightsailRegionsPromise = client.lightsail.getRegions().promise();
 
   return Promise.all([ec2RegionsPromise, lightsailRegionsPromise]).then(
-      ([ec2Regions, lightsailRegions]) => {
-        return ec2Regions.Regions.map((ec2Region) => {
-          const lsRegion = lightsailRegions.regions.find((x) => x.name === ec2Region.RegionName);
-          return lsRegion ?
-              { id: ec2Region.RegionName, value: `${ec2Region.RegionName} (${lsRegion.displayName})` } :
-              { id: ec2Region.RegionName, value: ec2Region.RegionName }
-        }).sort((a,b) => {
-          if (a.value > b.value) return 1;
-          if (a.value < b.value) return -1;
-          return 0;
-        });
+    ([ec2Regions, lightsailRegions]) => ec2Regions.Regions.map((ec2Region) => {
+      const lsRegion = lightsailRegions.regions.find((x) => x.name === ec2Region.RegionName);
+      return lsRegion
+        ? { id: ec2Region.RegionName, value: `${ec2Region.RegionName} (${lsRegion.displayName})` }
+        : { id: ec2Region.RegionName, value: ec2Region.RegionName };
+    }).sort((a, b) => {
+      if (a.value > b.value) {
+        return 1;
       }
+      if (a.value < b.value) {
+        return -1;
+      }
+      return 0;
+    }),
   ).catch((err) => {
     console.error(err);
     throw MISSING_OR_INCORRECT_CREDENTIALS_ERROR;
@@ -96,5 +110,5 @@ module.exports = {
   listRegions,
   listReposAuto: listAuto("listRepos", "repositories", ["repositoryName"]),
   listBranchesAuto: listAuto("listBranches", "branches"),
-  listPullRequestsAuto: listAuto("listPullRequests", "pullRequestIds")
-}
+  listPullRequestsAuto: listAuto("listPullRequests", "pullRequestIds"),
+};
