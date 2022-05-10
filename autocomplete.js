79ed5291a4d1370d7b997c195ec860b8898b915c
xkcm <xkcm16@gmail.com>
2022-05-10 15:53:44 +0200

    Refactor autocomplete functions

--- autocomplete.js
+++ autocomplete.js
@@ -1,54 +1,34 @@
 const awsPluginLibrary = require("kaholo-aws-plugin-library");
 const { fetchRecursively } = require("./helpers");
 
-async function listReposAuto(query, params, codeCommitClient) {
-  const fetchResult = await fetchRecursively(codeCommitClient, {
-    methodName: "listRepositories",
-    outputDataPath: "repositories",
-  }).catch((error) => {
-    throw new Error(`Failed to fetch repositories: ${error.message || JSON.stringify(error)}`);
-  });
+async function createAwsAutocompleteFunction(
+  methodName,
+  outputDataPath,
+  [valuePath, labelPath] = [],
+) {
+  return async (query, params, codeCommitClient) => {
+    const fetchResult = await fetchRecursively(codeCommitClient, {
+      methodName,
+      outputDataPath,
+    }).catch((error) => {
+      throw new Error(`Failed to list ${outputDataPath.toLowerCase()}: ${error.message || JSON.stringify(error)}`);
+    });
 
-  const mappedAutocompleteItems = fetchResult.map(({ repositoryName }) => (
-    awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(repositoryName)
-  ));
-  return awsPluginLibrary.autocomplete.filterItemsByQuery(mappedAutocompleteItems, query);
-}
+    const mappedAutocompleteItems = fetchResult.map((fetchedItem) => {
+      const autocompleteValue = valuePath ? fetchedItem[valuePath] : fetchedItem;
+      const autocompleteLabel = labelPath ? fetchedItem[labelPath] : autocompleteValue;
+      return awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(
+        autocompleteValue,
+        autocompleteLabel,
+      );
+    });
 
-async function listBranchesAuto(query, params, codeCommitClient) {
-  const fetchResult = await fetchRecursively(codeCommitClient, {
-    methodName: "listBranches",
-    outputDataPath: "branches",
-  }, {
-    repositoryName: params.repository,
-  }).catch((error) => {
-    throw new Error(`Failed to fetch branches: ${error.message || JSON.stringify(error)}`);
-  });
-
-  const mappedAutocompleteItems = fetchResult.map((branch) => (
-    awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(branch)
-  ));
-  return awsPluginLibrary.autocomplete.filterItemsByQuery(mappedAutocompleteItems, query);
-}
-
-async function listPullRequestsAuto(query, params, codeCommitClient) {
-  const fetchResult = await fetchRecursively(codeCommitClient, {
-    methodName: "listPullRequests",
-    outputDataPath: "pullRequestIds",
-  }, {
-    repositoryName: params.repository,
-  }).catch((error) => {
-    throw new Error(`Failed to fetch pull requests: ${error.message || JSON.stringify(error)}`);
-  });
-
-  const mappedAutocompleteItems = fetchResult.map((pullRequestId) => (
-    awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(pullRequestId)
-  ));
-  return awsPluginLibrary.autocomplete.filterItemsByQuery(mappedAutocompleteItems, query);
+    return awsPluginLibrary.autocomplete.filterItemsByQuery(mappedAutocompleteItems, query);
+  };
 }
 
 module.exports = {
-  listReposAuto,
-  listBranchesAuto,
-  listPullRequestsAuto,
+  listReposAuto: createAwsAutocompleteFunction("listRepositories", "repositories", ["repositoryName"]),
+  listBranchesAuto: createAwsAutocompleteFunction("listBranches", "branches"),
+  listPullRequestsAuto: createAwsAutocompleteFunction("listPullRequests", "pullRequestIds"),
 };
