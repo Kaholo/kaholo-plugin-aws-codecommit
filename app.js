38e47922d980e98f79170239e7a042cc5bf08154
maciek <42122011+xkcm@users.noreply.github.com>
2022-11-08 12:40:16 +0100

    KP-901 Display Pull Requests' titles in autocomplete & refactor (#14)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const AWS = require("aws-sdk");
 const awsPluginLibrary = require("@kaholo/aws-plugin-library");
+
+const codeCommitService = require("./code-commit-service");
 const payloadFunctions = require("./payload-functions");
 const autocomplete = require("./autocomplete");
-const { fetchRecursively } = require("./helpers");
 
 const simpleAwsMethods = {
   createRepository: awsPluginLibrary.generateAwsMethod("createRepository", payloadFunctions.prepareCreateRepositoryPayload),
@@ -11,38 +12,19 @@ const simpleAwsMethods = {
   getPullRequest: awsPluginLibrary.generateAwsMethod("getPullRequest", payloadFunctions.prepareGetPullRequestPayload),
 };
 
-async function listRepos(codeCommitClient) {
-  const repositories = await fetchRecursively(codeCommitClient, {
-    methodName: "listRepositories",
-    outputDataPath: "repositories",
-  }).catch((error) => {
-    throw new Error(`Failed to list repositories: ${error.message || JSON.stringify(error)}`);
-  });
+async function listRepos(codeCommitClient, params) {
+  const repositories = await codeCommitService.listRepos(codeCommitClient, params);
   return { repositories };
 }
 
 async function listBranches(codeCommitClient, params) {
-  const branches = await fetchRecursively(codeCommitClient, {
-    methodName: "listBranches",
-    outputDataPath: "branches",
-  }, {
-    repositoryName: params.repository,
-  }).catch((error) => {
-    throw new Error(`Failed to list branches: ${error.message || JSON.stringify(error)}`);
-  });
+  const branches = await codeCommitService.listBranches(codeCommitClient, params);
   return { branches };
 }
 
 async function listPullRequests(codeCommitClient, params) {
-  const pullRequestIds = await fetchRecursively(codeCommitClient, {
-    methodName: "listPullRequests",
-    outputDataPath: "pullRequestIds",
-  }, {
-    repositoryName: params.repository,
-  }).catch((error) => {
-    throw new Error(`Failed to list pull requests: ${error.message || JSON.stringify(error)}`);
-  });
-  return { pullRequestIds };
+  const pullRequests = await codeCommitService.listPullRequests(codeCommitClient, params);
+  return { pullRequests };
 }
 
 module.exports = awsPluginLibrary.bootstrap(
